623a036f666947437871dac133961f4728653939
Ingomar Otter <vax@kgbvax.net>
2015-11-06 21:17:33 +0100

    removed snippets

--- snippets/wifitest.lua
+++
@@ -1,9 +0,0 @@
-wifi.setmode(wifi.STATION)
-wifi.setphymode(wifi.PHYMODE_N)
-wifi.sleeptype(wifi.LIGHT_SLEEP)
-wifi.sta.config("Freifunk","")
-wifi.sta.connect()
-
-ip = wifi.sta.getip()
-print(ip)
-    
